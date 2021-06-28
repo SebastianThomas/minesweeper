16b2a718c525b86828d3d0dc9b34cb59708fec6f
SebastianThomas <sebastian_34260@web.de>
2021-06-28 21:34:28 +0200

    Implemented difficulties

--- main/java/de/sth/minesweeper/difficulties/Difficulties.java
+++ main/java/de/sth/minesweeper/difficulties/Difficulties.java
@@ -7,10 +7,17 @@ public enum Difficulties {
     PROFESSIONAL;
 
     public static Difficulty getDifficulty(Difficulties difficulty) {
-        if (difficulty == EASY) return new Difficulty(16, 16, 50);
-//            if (difficulty == MEDIUM) return new Difficulty(16, 16, 50);
-        if (difficulty == ADVANCED) return new Difficulty(24, 24, 99);
-//            if (difficulty == PROFESSIONAL) return new Difficulty(16, 16, 50);
+//        if (difficulty == EASY) return new Difficulty(9, 9, 35);
+        if (difficulty == EASY) return new Difficulty(9, 9, 10);
+
+//        if (difficulty == MEDIUM) return new Difficulty(16, 16, 99);
+        if (difficulty == MEDIUM) return new Difficulty(16, 16, 40);
+//        if (difficulty == ADVANCED) return new Difficulty(22, 22, 170);
+        if (difficulty == ADVANCED) return new Difficulty(22, 22, 99);
+
+
+//        if (difficulty == PROFESSIONAL) return new Difficulty(25, 25, 230);
+        if (difficulty == PROFESSIONAL) return new Difficulty(25, 25, 170);
         return null;
     }
 }
