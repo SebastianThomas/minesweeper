e6ca9ca42833b94074dc88a56eec165ad0aef777
SebastianThomas <sebastian_34260@web.de>
2021-06-29 16:24:53 +0200

    Timer .jar and version

--- main/java/de/sth/minesweeper/updates/UpdateConstants.java
+++ main/java/de/sth/minesweeper/updates/UpdateConstants.java
@@ -1,7 +1,7 @@
 package de.sth.minesweeper.updates;
 
 public abstract class UpdateConstants {
-    public static final String currentVersion = "v4.3";
+    public static final String currentVersion = "v4.4";
 
     enum UpdateOptions {
         /**
