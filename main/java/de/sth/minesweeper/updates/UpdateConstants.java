ee34d0fad3e844c56f4a615d844de07e0708727b
SebastianThomas <sebastian_34260@web.de>
2021-07-05 12:29:22 +0200

    Fixed bug with updates

--- main/java/de/sth/minesweeper/updates/UpdateConstants.java
+++ main/java/de/sth/minesweeper/updates/UpdateConstants.java
@@ -1,7 +1,7 @@
 package de.sth.minesweeper.updates;
 
 public abstract class UpdateConstants {
-    public static final String currentVersion = "v4.7-ea";
+    public static final String currentVersion = "v4.8-ea";
 
     enum UpdateOptions {
         /**
